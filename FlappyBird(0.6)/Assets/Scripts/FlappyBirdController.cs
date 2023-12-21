79231a48b4f7c56214de7b33cb61b339addf76a6
Furkna <erbilginfurkan@gmail.com>
2023-12-21 22:06:32 +0300

    Score multiplier eklendi. Glow effect eklendi.
    Speed boost aktifken karaktere glow efekti verildi. yeni bir score multiplier yeteneği eklendi. GameOver fonksiyonu LateUpdate'de çağırılması sağlandı.

--- FlappyBird(0.6)/Assets/Scripts/FlappyBirdController.cs
+++ FlappyBird(0.6)/Assets/Scripts/FlappyBirdController.cs
@@ -1,8 +1,9 @@
-using JetBrains.Annotations;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using TreeEditor;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -11,19 +12,23 @@ using UnityEngine.XR;
 public class FlappyBirdController : MonoBehaviour
 {
     float shieldDuration = 7.0f;
+    float multiplierDuration = 7.0f;
 
     public float jumpForce = 5f;
     public Text score_text;
     public TextMeshProUGUI creditsData;
     public int score;
 
-    public GameObject pipes, pipeUp, pipeDown;
-    public GameObject shieldItem;
+    public GameObject pipes,pipeUp,pipeDown;
+    public GameObject shieldItem,multiplierItem;
     [SerializeField] private GameObject forceField;
+    [SerializeField] private GameObject multSprite;
+    [SerializeField] private GameObject glowParticles; 
     [SerializeField] private Animator animator;
-    public bool inShield = false;
+    [HideInInspector] public bool inShield = false;
     [HideInInspector] public bool isSpeedy = false;
     [HideInInspector] public bool isFailed = false;
+    [HideInInspector] public bool isMultiplierOn = false;
 
     private void Start()
     {
@@ -43,7 +48,7 @@ public class FlappyBirdController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
         {
-            if (isSpeedy == false && isFailed == false)
+            if (isSpeedy==false && isFailed==false)
             {
                 Time.timeScale = 1f;
             }
@@ -52,7 +57,7 @@ public class FlappyBirdController : MonoBehaviour
                 GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpForce;
         }
 
-        if (isFailed == true && Input.GetKeyDown(KeyCode.R))
+        if (isFailed == true && Input.GetKeyDown(KeyCode.R)) 
         {
             SceneManager.LoadScene("FlappyBird");
         }
@@ -61,99 +66,117 @@ public class FlappyBirdController : MonoBehaviour
         creditsData.text = PlayerPrefs.GetInt("Total_Score", 0).ToString();
 
     }
+    private void LateUpdate()
+    {
+        if (isMultiplierOn == true){
+            multSprite.SetActive(true);
+            score_text.color = Color.yellow;
+        }
+        else{
+            multSprite.SetActive(false);
+            score_text.color= Color.white;
+        }
+        if (isFailed==true)
+        {
+            GameOver();
+        }
+        
+    }
 
     private void OnTriggerEnter2D(Collider2D temas)
     {
-        if (temas.gameObject.tag == "Scorer")
-        {
-            score++;
+        if (temas.gameObject.tag == "Scorer"){
+            if (isMultiplierOn == true)
+            {
+                score += 2;
+                Invoke("DeactivateMultiplier", multiplierDuration);
+            }
+            else
+                ++score;
         }
 
-        if (temas.gameObject.tag == "Speed")
-        {
+        if(temas.gameObject.tag == "Speed"){
             Destroy(temas.gameObject);
             Time.timeScale = 4f;
-            //score += 10;
             jumpForce = 0f;
             isSpeedy = true;
+            glowParticles.SetActive(true);
             GetComponent<Rigidbody2D>().velocity = Vector2.up * 0;
-            GetComponent<Rigidbody2D>().gravityScale = 0;
+            GetComponent<Rigidbody2D>().gravityScale = 0; 
 
             StartCoroutine(ResetSpeedBoost()); //ResetSpeedBoost fonksiyonunu çalýþtýrýr.
         }
 
-        if (temas.gameObject.tag == "Shield")
-        {
-            Destroy(temas.gameObject);
+        if(temas.gameObject.tag == "Shield"){
+            Destroy (temas.gameObject);
             forceField.SetActive(true);
             inShield = true;
-            Invoke("EndShieldSkill", shieldDuration);
+
+            Invoke("BreakShield", shieldDuration*Time.timeScale);
+        }
+        if (temas.gameObject.tag=="Multiplier"){
+            Destroy(temas.gameObject);
+            isMultiplierOn = true;
         }
     }
 
     private void OnCollisionEnter2D(Collision2D temas)
     {
-        if (temas.gameObject.tag == "Pipe")
-        {
-            if (inShield)
-            {
+        if (temas.gameObject.tag == "Pipe"){
+            if (inShield){
                 temas.collider.enabled = false; //shield aktifken temas edilen pipe'ýn collider'ýný kapatýr.
                 Invoke("BreakShield", 0);
             }
-            else
-            {
+            else{
                 isFailed = true;
-                PlayerPrefs.SetInt("Total_Score", PlayerPrefs.GetInt("Total_Score", 0) + score);
+                PlayerPrefs.SetInt("Total_Score", PlayerPrefs.GetInt("Total_Score",0)+score);
                 PlayerPrefs.Save();
-                GameOver();
+                Time.timeScale = 0;
+                
             }
         }
-        else if (temas.gameObject.tag == "Ground")
-        {
-            if (inShield)
-            {
+        else if (temas.gameObject.tag=="Ground"){
+            if (inShield){
                 Invoke("BreakShield", 0);
             }
-            else
-            {
+            else{
                 isFailed = true;
-                PlayerPrefs.SetInt("Total_Score", PlayerPrefs.GetInt("Total_Score", 0) + score);
+                PlayerPrefs.SetInt("Total_Score", PlayerPrefs.GetInt("Total_Score", 0) + score); //PlayerPrefs küçük boyutlu veri saklar. Oyun kapatýldýðýnda bile veri kaybedilmez. 
                 PlayerPrefs.Save();
-                GameOver();
+                Time.timeScale = 0;
             }
         }
     }
 
     private IEnumerator ResetSpeedBoost() //hýz boostu bittikten sonra her þeyi normale döndürür.
     {
-        yield return new WaitForSeconds(7.0f * Time.timeScale); //hýz boostunun 7 saniye sürmesini saðlar.
+        yield return new WaitForSeconds(7.0f*Time.timeScale); //hýz boostunun 7 saniye sürmesini saðlar.
 
         jumpForce = 5f;
         Time.timeScale = 1.0f;
         GetComponent<Rigidbody2D>().gravityScale = 2.0f;
 
         yield return new WaitForSeconds(1.0f); //hýz boostu bittikten sonra oyuncunun kendini toparlamasý için süre verilir.
-
+        
         isSpeedy = false;
+        glowParticles.SetActive(false);
     }
 
-    private void EndShieldSkill()
-    {
-        Invoke("BreakShield", 0);
-    }
     private void BreakShield()
     {
         animator.SetBool("IsShieldBroken", true);
 
         Invoke("TurnOffShield", 0.8f);
     }
-    private void TurnOffShield()
-    {
+    private void TurnOffShield() {
         animator.SetBool("IsShieldBroken", false);
         forceField.SetActive(false);
         inShield = false;
     }
-
+    private void DeactivateMultiplier() {
+        isMultiplierOn = false;
+        
+    }
     void GameOver()
     {
         // Game Over menüsünü etkinleþtir
@@ -162,6 +185,5 @@ public class FlappyBirdController : MonoBehaviour
         Time.timeScale = 0;
 
     }
-
 }
 
