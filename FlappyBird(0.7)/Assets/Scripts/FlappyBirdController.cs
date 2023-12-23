6ed100499d5066641300ba4176b33604202fd7ea
TULPARYUS <153992120+TULPARYUS@users.noreply.github.com>
2023-12-23 18:40:56 +0300

    Build Sorunu giderildi

--- FlappyBird(0.7)/Assets/Scripts/FlappyBirdController.cs
+++ FlappyBird(0.7)/Assets/Scripts/FlappyBirdController.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using TreeEditor;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
